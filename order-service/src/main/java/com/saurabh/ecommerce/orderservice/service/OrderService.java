df21571e37e41d48aba11c81ec161aae4a9ed51a
saurabh <guptasaurabh5858@gmail.com>
2024-11-26 00:18:38 +0530

    added

--- order-service/src/main/java/com/saurabh/ecommerce/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/saurabh/ecommerce/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.saurabh.ecommerce.orderservice.repository.OrderRepository;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,8 +19,9 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     public final OrderRepository orderRepository;
+    public final WebClient webClient;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         System.out.println(orderRequest.getOrderLineItemsDtoList());
@@ -28,7 +30,17 @@ public class OrderService {
                 .map(this:: mapTodo)
                 .toList();
         order.setOrderLineItemlist(orderLineItem);
-        orderRepository.save(order);
+          Boolean  result  = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+          if(result){
+              orderRepository.save(order);
+          }else{
+              throw new IllegalAccessException("Product is not in stock");
+          }
+
     }
 
     private OrderLineItem mapTodo(OrderLineItemDto orderLineItemDto) {
